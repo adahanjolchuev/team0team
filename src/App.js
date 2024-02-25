5d0d00597132f36933fac03b265dc858fe8c7f24
ElmirDev <147965924+elmirfadil@users.noreply.github.com>
2024-02-25 16:40:03 +0600

    addhome (#2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );

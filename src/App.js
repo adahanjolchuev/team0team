327d934099a9d21a7d8d0d9f20b10a361594fc69
ElmirDev <147965924+elmirfadil@users.noreply.github.com>
2024-02-25 16:58:20 +0600

    Addcenter (#3)
    * addhome
    
    * addcenter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
 import Home from "./components/pages/Home";
+import Center from "./components/pages/Center"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Center />
       <Footer />
     </div>
   );

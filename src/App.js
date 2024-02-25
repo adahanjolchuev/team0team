9587411499cc7bbf7ba95a8a7d0de5e5ae06a9b1
elmirfadil <pazilbekovelmirbek11@gmail.com>
2024-02-25 16:56:39 +0600

    addcenter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/header";
 import Home from "./components/pages/Home";
+import Center from "./components/pages/Center"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
+      <Center />
     </div>
   );
 }
